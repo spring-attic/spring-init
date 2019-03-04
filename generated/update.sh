68daff64198408506ee35a69253ddf76aa6c6d46
Dave Syer <dsyer@pivotal.io>
2019-03-04 14:34:02 +0000

    Update to Boot 2.1.2

--- generated/update.sh
+++ generated/update.sh
@@ -130,34 +130,34 @@ if ! [ -e $cache ]; then
     git clone https://github.com/spring-projects/spring-boot $cache
 fi
 
-(cd $cache; git checkout v2.1.1.RELEASE)
+(cd $cache; git fetch --tags && git checkout v2.1.2.RELEASE)
 
 src=$cache/spring-boot-project/spring-boot-autoconfigure
 tgt=`dirname $0`/autoconfigure
 init $tgt $src
-generate $src/pom.xml $tgt/pom.xml spring-boot-autoconfigure 2.1.1.BUILD-SNAPSHOT
+generate $src/pom.xml $tgt/pom.xml spring-boot-autoconfigure 2.1.2.BUILD-SNAPSHOT
 
 src=$cache/spring-boot-project/spring-boot-actuator-autoconfigure
 tgt=`dirname $0`/actuator
 init $tgt $src
-generate $src/pom.xml $tgt/pom.xml spring-boot-actuator-autoconfigure 2.1.1.BUILD-SNAPSHOT
+generate $src/pom.xml $tgt/pom.xml spring-boot-actuator-autoconfigure 2.1.2.BUILD-SNAPSHOT
 
 src=$cache/spring-boot-project/spring-boot-test-autoconfigure
 tgt=`dirname $0`/test
 init $tgt $src
-generate $src/pom.xml $tgt/pom.xml spring-boot-test-autoconfigure 2.1.1.BUILD-SNAPSHOT
+generate $src/pom.xml $tgt/pom.xml spring-boot-test-autoconfigure 2.1.2.BUILD-SNAPSHOT
 
 cache=`dirname $0`/sources/spring-security
 if ! [ -e $cache ]; then
     git clone https://github.com/spring-projects/spring-security $cache
 fi
 
-(cd $cache; git checkout 5.1.2.RELEASE)
+(cd $cache; git fetch --tags && git checkout 5.1.3.RELEASE)
 (cd $cache/config; ../gradlew install -x test)
 
 src=$cache/config
 tgt=`dirname $0`/security
 init $tgt $src
-generate $src/build/poms/pom-default.xml $tgt/pom.xml spring-security-config 5.1.2.BUILD-SNAPSHOT
+generate $src/build/poms/pom-default.xml $tgt/pom.xml spring-security-config 5.1.3.BUILD-SNAPSHOT
 
 
