ea36806cf7f0b7724b7fc9f516443d5af3c73c22
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-21 13:47:58 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * [ ] http://www.apache.org/licenses/ with 1 occurrences migrated to:
      https://www.apache.org/licenses/ ([https](https://www.apache.org/licenses/) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE-2.0 with 279 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).

--- samples/petclinic/src/test/java/org/springframework/samples/petclinic/bench/CacheBenchmarkIT.java
+++ samples/petclinic/src/test/java/org/springframework/samples/petclinic/bench/CacheBenchmarkIT.java
@@ -5,7 +5,7 @@ LazyInitBeanFactoryPostProcessor.java * Copyright 2016-2017 the original author 
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
