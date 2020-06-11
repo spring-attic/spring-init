f766add46b758b4404c287a5a0bc6a88e93f469e
Dave Syer <dsyer@pivotal.io>
2020-06-11 13:28:54 +0100

    Plus function sample

--- samples/function/src/main/java/com/example/FunctionApplication.java
+++ samples/function/src/main/java/com/example/FunctionApplication.java
@@ -3,35 +3,22 @@ package com.example;
 import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
-import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerFactoryAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.reactive.error.ErrorWebFluxAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.function.context.FunctionRegistration;
 import org.springframework.cloud.function.context.FunctionType;
-import org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration;
-import org.springframework.cloud.function.web.flux.ReactorAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.init.SpringInitApplication;
 
-@SpringInitApplication({ PropertyPlaceholderAutoConfiguration.class,
-		ConfigurationPropertiesAutoConfiguration.class, JacksonAutoConfiguration.class,
-		ReactiveWebServerFactoryAutoConfiguration.class, WebFluxAutoConfiguration.class,
-		ErrorWebFluxAutoConfiguration.class, HttpHandlerAutoConfiguration.class,
-		ContextFunctionCatalogAutoConfiguration.class, ReactorAutoConfiguration.class })
+@SpringBootApplication(proxyBeanMethods = false)
 public class FunctionApplication {
 
 	@Bean
 	public FunctionRegistration<Function<String, String>> uppercase() {
-		return new FunctionRegistration<Function<String, String>>(
-				value -> value.toUpperCase())
-						.type(FunctionType.from(String.class).to(String.class));
+		return new FunctionRegistration<Function<String, String>>(value -> value.toUpperCase())
+				.type(FunctionType.from(String.class).to(String.class));
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunctionApplication.class, args);
 	}
+
 }
