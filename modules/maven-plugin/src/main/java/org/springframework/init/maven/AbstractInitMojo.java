f43798258361ce43cf9013aa6390b1f375c74f81
Dave Syer <dsyer@pivotal.io>
2020-07-22 16:56:27 +0000

    Change goal name to testGenerate

---
+++ modules/maven-plugin/src/main/java/org/springframework/init/maven/AbstractInitMojo.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.init.maven;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+import org.springframework.boot.loader.tools.MainClassFinder;
+import org.springframework.init.tools.InitializerApplication;
+import org.springframework.util.ClassUtils;
+
+import com.squareup.javapoet.ClassName;
+
+/**
+ * @author dsyer
+ *
+ */
+public abstract class AbstractInitMojo extends AbstractMojo {
+
+	private static final String SPRING_BOOT_APPLICATION_CLASS_NAME = "org.springframework.boot.SpringBootConfiguration";
+	private static final String SPRING_INIT_APPLICATION_CLASS_NAME = "org.springframework.init.tools.InitializerApplication";
+	/**
+	 * The Maven project.
+	 * 
+	 * @since 1.0.0
+	 */
+	@Parameter(defaultValue = "${project}", readonly = true, required = true)
+	private MavenProject project;
+	/**
+	 * Skip the execution.
+	 * 
+	 * @since 1.3.2
+	 */
+	@Parameter(property = "spring-init.generate.skip", defaultValue = "false")
+	private boolean skip;
+	/**
+	 * Set the closed world flag - configuration can not be changed with classpath
+	 * changes or environment properties.
+	 * 
+	 * @since 1.3.2
+	 */
+	@Parameter(property = "spring-init.generate.closed-world", defaultValue = "false")
+	private boolean closedWorld;
+	/**
+	 * The name of the main class. If not specified the first compiled class found
+	 * that contains a 'main' method will be used.
+	 * 
+	 * @since 1.0.0
+	 */
+	@Parameter(property = "spring-init.generate.main-class")
+	private String mainClass;
+	/**
+	 * The name of the main class. If not specified the first compiled class found
+	 * that contains a 'main' method will be used.
+	 * 
+	 * @since 1.0.0
+	 */
+	@Parameter(property = "spring-init.generate.base-package")
+	private String basePackage;
+
+	public AbstractInitMojo() {
+		super();
+	}
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		if (this.skip) {
+			getLog().debug("skipping run as per configuration.");
+			return;
+		}
+		generate(getStart());
+		postProcess(project);
+	}
+
+	protected abstract void postProcess(MavenProject project);
+
+	private void generate(String start) throws MojoExecutionException {
+		ClassLoader realm = getClass().getClassLoader();
+		ClassLoader parent = realm instanceof ClassRealm ? ((ClassRealm) realm).getParentClassLoader()
+				: realm.getParent();
+		URLClassLoader loader = new URLClassLoader(getClassPathUrls(), parent);
+		ClassLoader original = null;
+		getLog().info("Project: " + project);
+		try {
+			if (closedWorld) {
+				getLog().info("Closed world");
+				System.setProperty("spring.init.closed-world", "true");
+			} else {
+				getLog().info("Open world");
+			}
+			original = ClassUtils.overrideThreadContextClassLoader(loader);
+			Class<?> type = loader.loadClass(SPRING_INIT_APPLICATION_CLASS_NAME);
+			getLog().info("Generating: " + start + " in: " + getOutputDirectory());
+			type.getMethod("main", String[].class).invoke(null,
+					new Object[] { new String[] { start, getOutputDirectory().getAbsolutePath() } });
+		} catch (Exception e) {
+			throw new MojoExecutionException("Cannot generate initializer class: " + SPRING_INIT_APPLICATION_CLASS_NAME,
+					e);
+		} finally {
+			System.setProperty("spring.init.closed-world", "false");
+			if (original != null) {
+				ClassUtils.overrideThreadContextClassLoader(original);
+			}
+		}
+	}
+
+	protected abstract File getOutputDirectory();
+
+	private String getStart() throws MojoExecutionException {
+		String mainClass = this.mainClass;
+		if (mainClass == null) {
+			if (this.basePackage == null) {
+				try {
+					mainClass = MainClassFinder.findSingleMainClass(getClassesDirectory(),
+							SPRING_BOOT_APPLICATION_CLASS_NAME);
+				} catch (IOException ex) {
+					throw new MojoExecutionException(ex.getMessage(), ex);
+				}
+			} else {
+				return this.basePackage;
+			}
+		}
+		if (mainClass == null) {
+			throw new MojoExecutionException("Unable to find a suitable main class, please add a 'mainClass' property");
+		}
+		return mainClass;
+	}
+
+	protected abstract File getClassesDirectory();
+
+	protected URL[] getClassPathUrls() throws MojoExecutionException {
+		try {
+			List<URL> urls = new ArrayList<>();
+			addProjectClasses(urls);
+			addDependencies(urls);
+			addTools(urls);
+			getLog().debug("Classpath: " + urls);
+			return urls.toArray(new URL[0]);
+		} catch (IOException ex) {
+			throw new MojoExecutionException("Unable to build classpath", ex);
+		}
+	}
+
+	private void addTools(List<URL> urls) {
+		if (!findJar(urls, "spring-init-tools")) {
+			urls.add(InitializerApplication.class.getProtectionDomain().getCodeSource().getLocation());
+		}
+		if (!findJar(urls, "javapoet")) {
+			urls.add(ClassName.class.getProtectionDomain().getCodeSource().getLocation());
+		}
+	}
+
+	private boolean findJar(List<URL> urls, String string) {
+		for (URL url : urls) {
+			if (url.toString().contains(string)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void addProjectClasses(List<URL> urls) throws MalformedURLException {
+		urls.add(this.getClassesDirectory().toURI().toURL());
+	}
+
+	private void addDependencies(List<URL> urls) throws MalformedURLException, MojoExecutionException {
+		Set<Artifact> artifacts = this.project.getArtifacts();
+		for (Artifact artifact : artifacts) {
+			if (artifact.getFile() != null) {
+				urls.add(artifact.getFile().toURI().toURL());
+			}
+		}
+	}
+
+}
--- modules/maven-plugin/src/main/java/org/springframework/init/maven/GenerateMojo.java
+++ modules/maven-plugin/src/main/java/org/springframework/init/maven/GenerateMojo.java
@@ -16,97 +16,15 @@
 package org.springframework.init.maven;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.toolchain.ToolchainManager;
-import org.codehaus.plexus.classworlds.realm.ClassRealm;
-import org.springframework.boot.loader.tools.MainClassFinder;
-import org.springframework.init.tools.InitializerApplication;
-import org.springframework.util.ClassUtils;
-
-import com.squareup.javapoet.ClassName;
 
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresProject = true, threadSafe = true, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
-public class GenerateMojo extends AbstractMojo {
-
-	private static final String SPRING_BOOT_APPLICATION_CLASS_NAME = "org.springframework.boot.SpringBootConfiguration";
-
-	private static final String SPRING_INIT_APPLICATION_CLASS_NAME = "org.springframework.init.tools.InitializerApplication";
-
-	/**
-	 * The Maven project.
-	 * 
-	 * @since 1.0.0
-	 */
-	@Parameter(defaultValue = "${project}", readonly = true, required = true)
-	private MavenProject project;
-
-	/**
-	 * The current Maven session. This is used for toolchain manager API calls.
-	 * 
-	 * @since 2.3.0
-	 */
-	@Parameter(defaultValue = "${session}", readonly = true)
-	private MavenSession session;
-
-	/**
-	 * The toolchain manager to use to locate a custom JDK.
-	 * 
-	 * @since 2.3.0
-	 */
-	@Component
-	private ToolchainManager toolchainManager;
-
-	/**
-	 * Skip the execution.
-	 * 
-	 * @since 1.3.2
-	 */
-	@Parameter(property = "spring-init.generate.skip", defaultValue = "false")
-	private boolean skip;
-
-	/**
-	 * Set the closed world flag - configuration can not be changed with classpath changes or environment properties.
-	 * 
-	 * @since 1.3.2
-	 */
-	@Parameter(property = "spring-init.generate.closed-world", defaultValue = "false")
-	private boolean closedWorld;
-
-	/**
-	 * The name of the main class. If not specified the first compiled class found
-	 * that contains a 'main' method will be used.
-	 * 
-	 * @since 1.0.0
-	 */
-	@Parameter(property = "spring-init.generate.main-class")
-	private String mainClass;
-
-	/**
-	 * The name of the main class. If not specified the first compiled class found
-	 * that contains a 'main' method will be used.
-	 * 
-	 * @since 1.0.0
-	 */
-	@Parameter(property = "spring-init.generate.base-package")
-	private String basePackage;
+public class GenerateMojo extends AbstractInitMojo {
 
 	/**
 	 * Directory containing the classes and resource files that should be scanned.
@@ -125,107 +43,17 @@ public class GenerateMojo extends AbstractMojo {
 	private File outputDirectory;
 
 	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		if (this.skip) {
-			getLog().debug("skipping run as per configuration.");
-			return;
-		}
-		generate(getStart());
+	protected void postProcess(MavenProject project) {
 		project.addCompileSourceRoot(outputDirectory.getAbsolutePath());
 	}
 
-	private void generate(String start) throws MojoExecutionException {
-		ClassLoader realm = getClass().getClassLoader();
-		ClassLoader parent = realm instanceof ClassRealm ? ((ClassRealm) realm).getParentClassLoader()
-				: realm.getParent();
-		URLClassLoader loader = new URLClassLoader(getClassPathUrls(), parent);
-		ClassLoader original = null;
-		getLog().info("Project: " + project);
-		try {
-			if (closedWorld) {
-				getLog().info("Closed world");
-				System.setProperty("spring.init.closed-world", "true");
-			} else {
-				getLog().info("Open world");
-			}
-			original = ClassUtils.overrideThreadContextClassLoader(loader);
-			Class<?> type = loader.loadClass(SPRING_INIT_APPLICATION_CLASS_NAME);
-			getLog().info("Generating: " + start + " in: " + outputDirectory);
-			type.getMethod("main", String[].class).invoke(null,
-					new Object[] { new String[] { start, outputDirectory.getAbsolutePath() } });
-		} catch (Exception e) {
-			throw new MojoExecutionException("Cannot generate initializer class: " + SPRING_INIT_APPLICATION_CLASS_NAME,
-					e);
-		} finally {
-			System.setProperty("spring.init.closed-world", "false");
-			if (original != null) {
-				ClassUtils.overrideThreadContextClassLoader(original);
-			}
-		}
+	@Override
+	protected File getOutputDirectory() {
+		return outputDirectory;
 	}
-
-	private String getStart() throws MojoExecutionException {
-		String mainClass = this.mainClass;
-		if (mainClass == null) {
-			if (this.basePackage == null) {
-				try {
-					mainClass = MainClassFinder.findSingleMainClass(this.classesDirectory,
-							SPRING_BOOT_APPLICATION_CLASS_NAME);
-				} catch (IOException ex) {
-					throw new MojoExecutionException(ex.getMessage(), ex);
-				}
-			} else {
-				return this.basePackage;
-			}
-		}
-		if (mainClass == null) {
-			throw new MojoExecutionException("Unable to find a suitable main class, please add a 'mainClass' property");
-		}
-		return mainClass;
+	
+	@Override
+	protected File getClassesDirectory() {
+		return classesDirectory;
 	}
-
-	protected URL[] getClassPathUrls() throws MojoExecutionException {
-		try {
-			List<URL> urls = new ArrayList<>();
-			addProjectClasses(urls);
-			addDependencies(urls);
-			addTools(urls);
-			getLog().debug("Classpath: " + urls);
-			return urls.toArray(new URL[0]);
-		} catch (IOException ex) {
-			throw new MojoExecutionException("Unable to build classpath", ex);
-		}
-	}
-
-	private void addTools(List<URL> urls) {
-		if (!findJar(urls, "spring-init-tools")) {
-			urls.add(InitializerApplication.class.getProtectionDomain().getCodeSource().getLocation());
-		}
-		if (!findJar(urls, "javapoet")) {
-			urls.add(ClassName.class.getProtectionDomain().getCodeSource().getLocation());
-		}
-	}
-
-	private boolean findJar(List<URL> urls, String string) {
-		for (URL url : urls) {
-			if (url.toString().contains(string)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private void addProjectClasses(List<URL> urls) throws MalformedURLException {
-		urls.add(this.classesDirectory.toURI().toURL());
-	}
-
-	private void addDependencies(List<URL> urls) throws MalformedURLException, MojoExecutionException {
-		Set<Artifact> artifacts = this.project.getArtifacts();
-		for (Artifact artifact : artifacts) {
-			if (artifact.getFile() != null) {
-				urls.add(artifact.getFile().toURI().toURL());
-			}
-		}
-	}
-
 }
